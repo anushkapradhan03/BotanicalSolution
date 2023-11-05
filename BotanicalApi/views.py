f37c1d2afc1abb88c622ab8c6f2a2b806a075276
anshumanprajapati <anshumanprajapati2000@gmail.com>
2023-11-05 17:15:54 +0530

    Object model

--- BotanicalApi/views.py
+++ BotanicalApi/views.py
@@ -10,6 +10,9 @@ from django.middleware.csrf import get_token
 from .models import Blog
 from BotanicalApi.mlModelsFunctions import predict_image, trainPlantPredictionModel
 
+from django.conf import settings
+
+
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import sys
@@ -20,21 +23,36 @@ import numpy as np
 import cv2
 
 # Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import TensorFlow1 as tf
+from TensorFlow1 import keras
+import tensorflow_hub as hub
+
+# from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+
+
+basePath = settings.BASE_DIR
 
 
 # Model saved with Keras model.save()
-diseaseDetectionAndSolution= r'D:\remoteGIThub\BotanicalSolution\BotanicalApi\mlModel\diseaseDetectionAndSolutionModel.h5'
+diseaseDetectionAndSolutionRelativePath = '/BotanicalApi/mlModel/diseaseDetectionAndSolutionModel.h5'
+datasetRelativePath = 'BotanicalApi/plantprdecitiondataset.csv'
+diseaseDetectionAndSolutionModelPath= os.path.join(basePath, diseaseDetectionAndSolutionRelativePath)
+datasetPath = os.path.join(basePath, datasetRelativePath)
 
 # # Load your trained model
-DiseaseDetectionAndSolutionModel = load_model(diseaseDetectionAndSolution)
+# DiseaseDetectionAndSolutionModel = load_model(diseaseDetectionAndSolution)
 
 def get_csrf_token(request):
     csrf_token = get_token(request)
     return JsonResponse({'csrfToken': csrf_token})
 
+# Function to load a saved .h5 model and make predictions
+def load_and_predict_h5_model(model_path):
+    # Load the saved model
+    model = keras.models.load_model(model_path)
+    return model
 
 def model_predict(img_path, model):
     
@@ -43,11 +61,15 @@ def model_predict(img_path, model):
     new_arr = cv2.resize(img,(100,100))
     new_arr = np.array(new_arr/255)
     new_arr = new_arr.reshape(-1, 100, 100, 3)
-    
 
-    
     preds = model.predict(new_arr)
     return preds
+
+
+
+
+
+#### Request Functions
 
 @csrf_exempt
 def process_image(request):
@@ -55,23 +77,8 @@ def process_image(request):
         # Save the image and enum value
         f = request.FILES.get('image')
         enum_value = request.POST.get('enum')
-        #f = request.files['image']
         print(f)
-        #print(image)
-        #f = image
-        #f.filename = 'detectThis'
 
-        # Perform any necessary processing or saving of the image
-        # Save the image temporarily
-        #image_path = '/path/to/save/image.png'  # Replace with the actual path to save the image
-
-        # with open(image_path, 'wb') as file:
-        #     for chunk in image.chunks():
-        #         file.write(chunk)
-
-        # Call the predict_image function from ml_model.py
-
-        
 
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
@@ -81,6 +88,7 @@ def process_image(request):
         #file_path = r'D:\remoteGIThub\BotanicalSolution\BotanicalApi\uploads\PaperBellBact Spot.JPG'
 
         # Make prediction
+        DiseaseDetectionAndSolutionModel = load_and_predict_h5_model(diseaseDetectionAndSolutionModelPath)
         preds = model_predict(file_path, DiseaseDetectionAndSolutionModel)
 
         # Process your result for human
@@ -98,8 +106,6 @@ def process_image(request):
         prediction = CATEGORIES[pred_class]
 
 
-        #prediction = predict_image(image_path)
-
         # Return the prediction result or any other response
         return JsonResponse({'message': 'Image processed successfully', 'prediction': prediction})
     else:
@@ -115,8 +121,8 @@ def plant_suggestion(request):
         print(temperature +" "+ precipitation+" "+elevation)
 
         # Load the dataset
-        dataset_path = 'D:\minorProject\BotanicalSolution\BotanicalApi\plantprdecitiondataset.csv'
-        dataset = pd.read_csv(dataset_path)
+       
+        dataset = pd.read_csv(datasetPath)
         print(dataset)
 
         # Split the dataset into input features (X) and target variable (y)
@@ -137,88 +143,88 @@ def plant_suggestion(request):
         return JsonResponse({'error': 'Invalid request method'})
 
 
-@csrf_exempt
-def post_blog(request):
-    if request.method == 'POST':
-        # Retrieve the blog details from the request
-        title = request.POST.get('title')
-        short_description = request.POST.get('short_description')
-        detailed_paragraph = request.POST.get('detailed_paragraph')
-        image = request.FILES.get('image')
-        user_id = request.POST.get('user_id')
-        star_ratings = request.POST.get('star_ratings')
+# @csrf_exempt
+# def post_blog(request):
+#     if request.method == 'POST':
+#         # Retrieve the blog details from the request
+#         title = request.POST.get('title')
+#         short_description = request.POST.get('short_description')
+#         detailed_paragraph = request.POST.get('detailed_paragraph')
+#         image = request.FILES.get('image')
+#         user_id = request.POST.get('user_id')
+#         star_ratings = request.POST.get('star_ratings')
 
-        # Retrieve the User instance
-        user = get_object_or_404(User, id=user_id)
+#         # Retrieve the User instance
+#         user = get_object_or_404(User, id=user_id)
 
-        # Create a new blog instance
-        blog = Blog(
-            title=title,
-            short_description=short_description,
-            detailed_paragraph=detailed_paragraph,
-            user=user,
-            star_ratings=star_ratings
-        )
+#         # Create a new blog instance
+#         blog = Blog(
+#             title=title,
+#             short_description=short_description,
+#             detailed_paragraph=detailed_paragraph,
+#             user=user,
+#             star_ratings=star_ratings
+#         )
 
-        # If an image is provided, assign it to the blog instance
-        if image:
-            blog.image = image
+#         # If an image is provided, assign it to the blog instance
+#         if image:
+#             blog.image = image
 
-        # Save the blog
-        blog.save()
+#         # Save the blog
+#         blog.save()
 
-        return JsonResponse({'message': 'Blog posted successfully'})
-    else:
-        return JsonResponse({'error': 'Invalid request method'})
+#         return JsonResponse({'message': 'Blog posted successfully'})
+#     else:
+#         return JsonResponse({'error': 'Invalid request method'})
 
-def get_blogs(request):
-    blogs = Blog.objects.all()
+# def get_blogs(request):
+#     blogs = Blog.objects.all()
 
-    blog_data = []
-    for blog in blogs:
-        blog_data.append({
-            'title': blog.title,
-            'short_description': blog.short_description,
-            'detailed_paragraph': blog.detailed_paragraph,
-            'image': blog.image.url if blog.image else None,
-            'user': blog.user.username,
-            'star_ratings': blog.star_ratings,
-            'posted_date': blog.posted_date.strftime('%Y-%m-%d'),
-        })
+#     blog_data = []
+#     for blog in blogs:
+#         blog_data.append({
+#             'title': blog.title,
+#             'short_description': blog.short_description,
+#             'detailed_paragraph': blog.detailed_paragraph,
+#             'image': blog.image.url if blog.image else None,
+#             'user': blog.user.username,
+#             'star_ratings': blog.star_ratings,
+#             'posted_date': blog.posted_date.strftime('%Y-%m-%d'),
+#         })
 
-    return JsonResponse({'blogs': blog_data})
+#     return JsonResponse({'blogs': blog_data})
 
+# @csrf_exempt
+# def signin(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
 
-@csrf_exempt
-def signin(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return JsonResponse({'message': 'Sign in successful'})
+#         else:
+#             return JsonResponse({'error': 'Invalid credentials'})
+#     else:
+#         return JsonResponse({'error': 'Invalid request method'})
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return JsonResponse({'message': 'Sign in successful'})
-        else:
-            return JsonResponse({'error': 'Invalid credentials'})
-    else:
-        return JsonResponse({'error': 'Invalid request method'})
+# @csrf_exempt
+# def signup(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
 
-@csrf_exempt
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+#         if username and password:
+#             # Create a new user
+#             user = User.objects.create_user(username=username, password=password)
+#             return JsonResponse({'message': 'Sign up successful'})
+#         else:
+#             return JsonResponse({'error': 'Username and password are required'})
+#     else:
+#         return JsonResponse({'error': 'Invalid request method'})
 
-        if username and password:
-            # Create a new user
-            user = User.objects.create_user(username=username, password=password)
-            return JsonResponse({'message': 'Sign up successful'})
-        else:
-            return JsonResponse({'error': 'Username and password are required'})
-    else:
-        return JsonResponse({'error': 'Invalid request method'})
-
-def signout(request):
+# def signout(request):
     logout(request)
     return JsonResponse({'message': 'Logged out successfully'})
+
